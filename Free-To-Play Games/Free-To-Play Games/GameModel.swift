f22b63720e4c5bfa98e7cdb13c89e311109b0c5f
Quinten De Wolf <quinten.dewolf2@student.hogent.be>
2023-10-28 16:22:05 +0200

    Changed model to include category

--- Free-To-Play Games/Free-To-Play Games/GameModel.swift
+++ Free-To-Play Games/Free-To-Play Games/GameModel.swift
@@ -10,14 +10,23 @@ import Foundation
 struct GameModel {
     private (set) var platforms: Array<Platform>
     private (set) var selectedPlatform: Platform = Platform.pc
+    private (set) var categories: Array<Category>
+    private (set) var selectedCategory: Category = Category.moba
     
     init() {
         platforms = []
+        categories = []
         for platform in Platform.allCases {
             platforms.append(platform)
+        }
+        for category in Category.allCases {
+            categories.append(category)
         }
     }
     mutating func selectPlatform(_ platform: Platform) {
         selectedPlatform = platform
+    }
+    mutating func selectCategory(_ category: Category) {
+        selectedCategory = category
     }
 }

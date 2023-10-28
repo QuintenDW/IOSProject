0f7b470fdba361a7eeffd233b567829ec45520f7
Quinten De Wolf <quinten.dewolf2@student.hogent.be>
2023-10-28 12:29:44 +0200

    Setup project

---
+++ Free-To-Play Games/Free-To-Play Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Free-To-Play Games
+//
+//  Created by Quinten De Wolf on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Free-To-Play Games/Free-To-Play Games/Free_To_Play_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Free_To_Play_GamesApp.swift
+//  Free-To-Play Games
+//
+//  Created by Quinten De Wolf on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Free_To_Play_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

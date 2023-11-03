5510e685cb9d623651ab13a1b6615e4687861d57
Quinten De Wolf <quinten.dewolf2@student.hogent.be>
2023-11-03 16:27:11 +0100

    Created Game model

---
+++ Free-To-Play Games/Free-To-Play Games/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  Free-To-Play Games
+//
+//  Created by Quinten De Wolf on 03/11/2023.
+//
+
+import Foundation
+
+struct Game: Identifiable {
+    let id = UUID()
+    var name: String
+    var description: String
+    var genre: Category
+    var platform: Platform
+    var developer: String
+    
+    static var builtin = { [
+        Game(name: "Overwatch 2", description: "A hero-focussed first-person team shooter from Blizzard Entertainment", genre: Category.shooter,platform: Platform.pc,developer: "Blizzard Entertainment")
+        Game(name: "Diablo Immortal", description: "Diablo Immortal fills in the gaps between Diablo 2 and 3", genre: Category.mmorpg,platform: Platform.pc,developer: "Blizzard Entertainment") ]
+    }
+}
